eb546379a92fe084d329285615866540be4dad42
rumunyana <113432535+rumunyana@users.noreply.github.com>
2023-06-27 23:55:58 +0200

    you

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 """0-pascal_triangle.py"""
 
+
 def pascal_triangle(n):
+    """Print a pascal triangle"""
     if n <= 0:
         return []
-    
     triangle = [[1]]
-    
     for i in range(1, n):
         row = [1]
-        prev_row = triangle[i-1]
-        
         for j in range(1, i):
-            row.append(prev_row[j-1] + prev_row[j])
-        
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
         row.append(1)
         triangle.append(row)
-    
     return triangle
